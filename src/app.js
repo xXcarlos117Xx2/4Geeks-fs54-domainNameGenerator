3658e3065e36433ebe55f398f267ef3ade8e3078
Carlos Atanes Vences <carlos117g@gmail.com>
2023-12-07 17:38:44 +0000

    adios prettier

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const words = {
+    pronoun: ["the", "our"],
+    adj: ["great", "big"],
+    noun: ["jogger", "racoon"]
+  };
+  let result = "";
+  for (let i = 0; i < Object.keys(words).length; i++) {
+    result +=
+      words[Object.keys(words)[i]][
+        Math.floor(Math.random() * words[Object.keys(words)[i]].length)
+      ];
+    // if (i < Object.keys(words).length - 1) {
+    //   result += " ";
+    // }
+  }
+
+  document.getElementById("domain").innerHTML = result;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const PrettierPlugin = require("./_utils/prettier.js");
+
 const cleanStack = require("./_utils/clean-stack.js");
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
@@ -73,9 +73,6 @@ module.exports = {
     new HtmlWebpackPlugin({
         filename: "index.html",
         template: "src/index.html"
-    }),
-    new PrettierPlugin({
-        failSilently: true
-    }),
+    })
   ]
 };
